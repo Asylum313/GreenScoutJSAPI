584942964d2d3ad42455812f712490db7684b481
Awesome65432139 <104233315+AWESOME654321@users.noreply.github.com>
2025-12-15 19:33:07 -0600

    v1.0 actually fixed

--- greenscoutjs/src/main.jsx
+++ greenscoutjs/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom"
-import { ReactDOM } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <BrowserRouter> */}
+    <BrowserRouter>
       <App/>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </StrictMode>,
 );
